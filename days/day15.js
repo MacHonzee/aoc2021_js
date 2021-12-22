ece293b652cf107fb8d6bec17df44dec9f7c4c77
Jan Rudolf <jan.rudolf@plus4u.net>
2021-12-22 08:56:03 +0100

    Day 15, no Dijkstra or A* so it does not work :)

--- days/day15.js
+++ days/day15.js
@@ -6,62 +6,59 @@ const MAX_X = map[0].length - 1;
 const MAX_Y = map.length - 1;
 // magic constant, bigger => worse performance but more optimalization
 // for example MAX_DEPTH = 3 means that the algorithm will check best route for 3 steps ahead
-const MAX_DEPTH = 10;
+const MAX_DEPTH = 25;
 const END = [MAX_X, MAX_Y];
 
-function getAdjacentCoords(position, excludedPath, takenPath) {
+const RISK_MAP = map.reduce((final, line, y) => {
+  line.forEach((risk, x) => {
+    final[`${x},${y}`] = risk;
+  });
+  return final;
+}, {});
+
+function getNextCoords (position, excludedPath, takenPath) {
   let [x, y] = position;
   let coords = [];
+  let strCoords = [];
 
-  if (x > 0) coords.push([x - 1, y]);
-  if (x < MAX_X) coords.push([x + 1, y]);
-  if (y > 0) coords.push([x, y - 1]);
-  if (y < MAX_Y) coords.push([x, y + 1]);
-
-  coords = coords.filter(coo => {
-    let strCoo = coo.join(",");
-    return !excludedPath.has(strCoo) && !takenPath.has(strCoo);
-  });
-
-  return coords;
-}
-
-function expandPath(position, currentPath, allPaths, takenPath) {
-  if (currentPath.size > MAX_DEPTH || (position[0] === END[0] && position[1] === END[1])) {
-    allPaths.push(currentPath);
-    return allPaths;
+  let leftStr = `${x + 1},${y}`;
+  let bottomStr = `${x},${y + 1}`;
+  if (x < MAX_X && !excludedPath.has(leftStr) && !takenPath.has(leftStr)) {
+    coords.push([x + 1, y]);
+    strCoords.push(leftStr);
+  }
+  if (y < MAX_Y && !excludedPath.has(bottomStr) && !takenPath.has(bottomStr)) {
+    coords.push([x, y + 1]);
+    strCoords.push(bottomStr);
   }
 
-  let adjacentCoords = getAdjacentCoords(position, currentPath, takenPath);
-  adjacentCoords.forEach(coords => {
-    let newPath = new Set(currentPath);
-    newPath.add(coords.join(","));
-    expandPath(coords, newPath, allPaths, takenPath);
+  return { coords, strCoords };
+}
+
+function expandPath (position, currentPath, lowestRiskPath, takenPath) {
+  if (currentPath.size > MAX_DEPTH || (position[0] === END[0] && position[1] === END[1])) {
+    let pathRisk = getRiskOfPath(currentPath);
+    if (!lowestRiskPath.risk || lowestRiskPath.risk > pathRisk) {
+      lowestRiskPath.risk = pathRisk;
+      lowestRiskPath.path = currentPath;
+    }
+    return lowestRiskPath;
+  }
+
+  let { coords: nextCoords, strCoords } = getNextCoords(position, currentPath, takenPath);
+  nextCoords.forEach((coords, i) => {
+    let newPath = new Set([...currentPath, strCoords[i]]);
+    expandPath(coords, newPath, lowestRiskPath, takenPath);
   });
 
-  return allPaths;
+  return lowestRiskPath;
 }
 
-function getRiskOfPath(path) {
-  return Array.from(path).reduce((sum, coords) => {
-    let [x, y] = coords.split(",").map(Number);
-    sum += map[y][x];
-    return sum;
-  }, 0);
+function getRiskOfPath (path) {
+  return Array.from(path).reduce((sum, coords) => sum + RISK_MAP[coords], 0);
 }
 
-function findLowestRiskPath(paths) {
-  let allRisks = paths.map(getRiskOfPath);
-
-  let indexOfLowest = 0;
-  allRisks.forEach((risk, i) => {
-    if (allRisks[indexOfLowest] > risk) indexOfLowest = i;
-  });
-
-  return paths[indexOfLowest];
-}
-
-function drawPath(path) {
+function drawPath (path) {
   map.forEach((line, y) => {
     let str = "";
     line.forEach((risk, x) => {
@@ -76,22 +73,21 @@ function drawPath(path) {
   console.log("\n");
 }
 
-let current = [0,0];
+let current = [0, 0];
 let takenPath = new Set();
 while (!(current[0] === END[0] && current[1] === END[1])) {
+  console.log(current);
 
-  let possiblePaths = expandPath(current, new Set([current.join(",")]), [], takenPath);
+  let lowestRiskPath = expandPath(current, new Set([current.join(",")]), {}, takenPath);
 
-  let lowestRiskPath = findLowestRiskPath(possiblePaths);
-
-  current = Array.from(lowestRiskPath).pop().split(",").map(Number);
-  takenPath = new Set([...takenPath, ...lowestRiskPath]);
+  current = Array.from(lowestRiskPath.path).pop().split(",").map(Number);
+  takenPath = new Set([...takenPath, ...lowestRiskPath.path]);
 }
 
 drawPath(takenPath);
 
-console.log(takenPath);
-let excludeStartAndEnd = Array.from(takenPath);
-excludeStartAndEnd.shift();
-excludeStartAndEnd.pop();
-console.log(getRiskOfPath(excludeStartAndEnd));
+let finalRisk = getRiskOfPath(takenPath);
+finalRisk -= map[0][0];
+// 405 ani 535 to není (answer was too high)
+console.log(finalRisk);
+
