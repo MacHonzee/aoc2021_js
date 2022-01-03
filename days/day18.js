a127c90bffb69849ae84ecb6472263328908ea0f
Jan Rudolf <jan.rudolf@plus4u.net>
2022-01-03 13:04:01 +0100

    Day 18 does not work on more complex scenario, works on simpler ones.

--- days/day18.js
+++ days/day18.js
@@ -25,7 +25,43 @@ class Pair {
   }
 }
 
-let rootPair = new Pair(input[0], input[1], null, 0);
+function findNeighbouringNumber(originalPair, targetSide) {
+  let parentPair = originalPair.parent;
+  let currentPair = originalPair;
+  while (parentPair) {
+    // if there is a number, we need that pair
+    if (typeof parentPair[targetSide] === "number") {
+      return { side: targetSide, pair: parentPair };
+    } else {
+      // otherwise it is an array, so we need to check if it is in a different coord
+      let parentCoord = parentPair.children.x === currentPair ? "x" : "y";
+
+      // in case that we need left neighbour (targetSide === x), then we have to see if
+      // previous pair is in same coord or not. If not (for example it is currently in Y position),
+      // then we need to find top-most number in targetSide of the current pair
+      // otherwise we traverse deeper into parent structure
+      if (parentCoord === targetSide) {
+        currentPair = parentPair;
+        parentPair = parentPair.parent;
+      } else {
+        let oppositeSide = targetSide === "x" ? "y" : "x";
+        let topMostPair = findTopMostNumber(parentPair.children[targetSide], oppositeSide);
+        return { side: oppositeSide, pair: topMostPair };
+      }
+    }
+  }
+}
+
+function findTopMostNumber(pair, targetSide) {
+  let currentPair = pair;
+  while (currentPair) {
+    if (typeof currentPair[targetSide] === "number") {
+      return currentPair;
+    } else {
+      currentPair = currentPair.children[targetSide];
+    }
+  }
+}
 
 function explodePair(pair) {
   // find out what was the initial index in structure
@@ -33,18 +69,15 @@ function explodePair(pair) {
   let parentCoord = parent.children.x === pair ? "x" : "y";
 
   // find closest left neighbour and add the original number
-  // FIXME nope not like this
-  let currentPair = pair.parent;
-  while (currentPair) {
-    if (typeof currentPair.x === "number") {
-      currentPair.x += pair.x;
-      break;
-    } else if (typeof currentPair.y === "number") {
-      currentPair.y += pair.y;
-      break;
-    } else {
-      currentPair = currentPair.parent;
-    }
+  let leftPair = findNeighbouringNumber(pair, "x");
+  if (leftPair) {
+    leftPair.pair[leftPair.side] += pair.x;
+  }
+
+  // find closest right neighbour and add the original number
+  let rightPair = findNeighbouringNumber(pair, "y");
+  if (rightPair) {
+    rightPair.pair[rightPair.side] += pair.y;
   }
 
   // replace pair by 0 and fix the children reference of parent
@@ -52,22 +85,85 @@ function explodePair(pair) {
   parent[parentCoord] = 0;
 }
 
-function splitPair(pair) {}
+function splitPair(pair, targetSide) {
+  let half = pair[targetSide] / 2;
+  let newX = Math.floor(half);
+  let newY = Math.ceil(half);
+  pair[targetSide] = [newX, newY];
+  pair.children[targetSide] = new Pair(newX, newY, pair, pair.nesting + 1);
+}
 
-function reducePair(pair) {
-  if (pair.nesting >= 4) { // TODO maybe check pair.children first
+function checkAllExplodes(pair) {
+  if (!pair.children.x && !pair.children.y && pair.nesting >= 4) {
     explodePair(pair);
+    return "exploding";
   }
 
-  if (pair.x > 9 || pair.y > 10) {
-    splitPair(pair);
+  if (pair.children && pair.children.x) {
+    let exploding = checkAllExplodes(pair.children.x);
+    if (exploding) return exploding;
   }
 
-  if (pair.children) {
-    pair.children.x && reducePair(pair.children.x);
-    pair.children.y && reducePair(pair.children.y);
+  if (pair.children && pair.children.y) {
+    let exploding = checkAllExplodes(pair.children.y);
+    if (exploding) return exploding;
   }
 }
 
-reducePair(rootPair);
-console.log("-> rootPair", rootPair);
+function checkAllSplits(pair) {
+  if (pair.x > 9) {
+    splitPair(pair, "x");
+    return "splitting";
+  }
+
+  if (pair.y > 9) {
+    splitPair(pair, "y");
+    return "splitting";
+  }
+
+  if (pair.children && pair.children.x) {
+    let splitting = checkAllSplits(pair.children.x);
+    if (splitting) return splitting;
+  }
+
+  if (pair.children && pair.children.y) {
+    let splitting = checkAllSplits(pair.children.y);
+    if (splitting) return splitting;
+  }
+}
+
+function drawPair(pair) {
+  let isXnumber = typeof pair.x === "number";
+  let isYnumber = typeof pair.y === "number";
+  return `[${isXnumber ? pair.x : drawPair(pair.children.x)},${isYnumber ? pair.y : drawPair(pair.children.y)}]`;
+}
+
+function addAllLines(input) {
+  let targetArray = input[0];
+  for (let i = 1; i < input.length; i++) {
+    // processing addition to previous line
+    targetArray = [targetArray, input[i]];
+    let rootPair = new Pair(targetArray[0], targetArray[1], null, 0);
+    console.log("-> after addition ", drawPair(rootPair));
+
+    // then we reduce the newly added pair
+    let reducing;
+    do {
+      reducing = checkAllExplodes(rootPair); // all explodes first
+      if (!reducing) reducing = checkAllSplits(rootPair); // all splits first
+      if (reducing) console.log("-> after " + reducing, drawPair(rootPair));
+    } while (reducing);
+    console.log("-> after reducing ", drawPair(rootPair));
+
+    // and create new array for next step out of the result
+    targetArray = JSON.parse(drawPair(rootPair));
+    console.log();
+  }
+
+  let rootPair = new Pair(targetArray[0], targetArray[1], null, 0);
+  console.log("-> final result   ", drawPair(rootPair));
+  return rootPair;
+}
+
+addAllLines(input);
+
