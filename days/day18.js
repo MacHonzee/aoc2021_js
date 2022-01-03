1bdf868807a15752a18001746598437c3c8a8c6d
Jan Rudolf <jan.rudolf@plus4u.net>
2022-01-03 16:29:25 +0100

    Day 18 solved! Thanks reddit, they found the bug in splitting function. Magnitude and part two was easy after that.

--- days/day18.js
+++ days/day18.js
@@ -1,5 +1,7 @@
 const input = require("../inputs/day18");
 
+const VERBOSE = false;
+
 class Pair {
   constructor(x, y, parent, nesting) {
     this.x = x;
@@ -111,22 +113,22 @@ function checkAllExplodes(pair) {
 }
 
 function checkAllSplits(pair) {
-  if (pair.x > 9) {
-    splitPair(pair, "x");
-    return "splitting";
-  }
-
-  if (pair.y > 9) {
-    splitPair(pair, "y");
-    return "splitting";
-  }
-
-  if (pair.children && pair.children.x) {
+  if (typeof pair.x === "number") {
+    if (pair.x > 9) {
+      splitPair(pair, "x");
+      return "splitting";
+    }
+  } else {
     let splitting = checkAllSplits(pair.children.x);
     if (splitting) return splitting;
   }
 
-  if (pair.children && pair.children.y) {
+  if (typeof pair.y === "number") {
+    if (pair.y > 9) {
+      splitPair(pair, "y");
+      return "splitting";
+    }
+  } else {
     let splitting = checkAllSplits(pair.children.y);
     if (splitting) return splitting;
   }
@@ -144,26 +146,56 @@ function addAllLines(input) {
     // processing addition to previous line
     targetArray = [targetArray, input[i]];
     let rootPair = new Pair(targetArray[0], targetArray[1], null, 0);
-    console.log("-> after addition ", drawPair(rootPair));
+    if (VERBOSE) console.log("-> after addition ", drawPair(rootPair));
 
     // then we reduce the newly added pair
     let reducing;
     do {
       reducing = checkAllExplodes(rootPair); // all explodes first
       if (!reducing) reducing = checkAllSplits(rootPair); // all splits first
-      if (reducing) console.log("-> after " + reducing, drawPair(rootPair));
+      if (reducing && VERBOSE) console.log("-> after " + reducing, drawPair(rootPair));
     } while (reducing);
-    console.log("-> after reducing ", drawPair(rootPair));
+    if (VERBOSE) console.log("-> after reducing ", drawPair(rootPair));
 
     // and create new array for next step out of the result
     targetArray = JSON.parse(drawPair(rootPair));
-    console.log();
+    if (VERBOSE) console.log();
   }
 
   let rootPair = new Pair(targetArray[0], targetArray[1], null, 0);
-  console.log("-> final result   ", drawPair(rootPair));
+  if (VERBOSE) console.log("-> final result   ", drawPair(rootPair));
   return rootPair;
 }
 
-addAllLines(input);
+function magnitude(pair) {
+  let magnitudeX = typeof pair.x === "number" ? 3 * pair.x : 3 * magnitude(pair.children.x);
+  let magnitudeY = typeof pair.y === "number" ? 2 * pair.y : 2 * magnitude(pair.children.y);
+  return magnitudeX + magnitudeY;
+}
 
+// part one
+let finalPair = addAllLines(input);
+console.log(magnitude(finalPair));
+
+// part two
+function combineToAllPairs(input) {
+  let allCombos = {};
+  for (let i = 0; i < input.length; i++) {
+    for (let y = 0; y < input.length; y++) {
+      if (i !== y) {
+        allCombos[`${i},${y}`] = 0;
+      }
+    }
+  }
+  return allCombos;
+}
+
+let allCombos = combineToAllPairs(input);
+Object.keys(allCombos).forEach(combo => {
+  let [i0, i1] = combo.split(",");
+  let lines = [input[i0], input[i1]];
+  let pair = addAllLines(lines);
+  allCombos[combo] = magnitude(pair);
+});
+
+console.log(Math.max(...Object.values(allCombos)));
